b72e38505def8316f1cce67a4bdaa45929e9ab66
junaidiiith <junaid695683@gmail.com>
2018-10-26 22:57:10 +0530

    Final commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,8 @@
 PORT=8000
 
 # Run migrations
+python manage.py stream &
+python manage.py makemigrations
 python manage.py migrate
 # Load the database with fixtures
-python manage.py loaddata users posts comments
-
 python manage.py runserver ${PORT}
